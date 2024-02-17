649383bca3d874c6c3b142a9c05aa979d498c273
rim-777 <rails.moss@gmail.com>
2024-02-17 18:39:19 +0000

    add logic for pinging a server

--- lib/echo_ping_pong.ex
+++ lib/echo_ping_pong.ex
@@ -5,4 +5,22 @@ defmodule EchoPingPong do
   Documentation for `EchoPingPong`.
   The server accepts the message :ping and echoes {:pong, node()} back to the client.
   """
+
+  def start_link() do
+    GenServer.start_link(__MODULE__, [])
+  end
+
+  def call(pid) do
+    GenServer.call(pid, :ping)
+  end
+
+  @impl GenServer
+  def init(_args) do
+    {:ok, nil}
+  end
+
+  @impl GenServer
+  def handle_call(:ping, _from, state) do
+    {:reply, {:pong, node()}, state}
+  end
 end
--- test/echo_ping_pong_test.exs
+++ test/echo_ping_pong_test.exs
@@ -1,3 +1,10 @@
 defmodule EchoPingPongTest do
   use ExUnit.Case
+  doctest EchoPingPong
+
+  test "pings a server" do
+    {:ok, _pid} = Node.start(:test@localhost)
+    {:ok, pid} = EchoPingPong.start_link()
+    assert EchoPingPong.call(pid) == {:pong, :test@localhost}
+  end
 end
